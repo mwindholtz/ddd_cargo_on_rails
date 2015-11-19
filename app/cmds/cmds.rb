36ff7e8c1929e634d415813f8fabc66d36fd3fb4
Mark Windholtz <mark@agiledna.com>
2015-11-19 18:57:46 -0500

    sorted list of cmds

--- app/cmds/cmds.rb
+++ app/cmds/cmds.rb
@@ -1,10 +1,10 @@
 module Cmds
 end             
 require 'base'
+require 'cargo_create'
+require 'cargo_destroy'
 require 'cargo_find'
 require 'cargo_query'
-require 'cargo_create'
 require 'cargo_update'
-require 'cargo_destroy'
 
 
