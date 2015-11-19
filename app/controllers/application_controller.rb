81552a100409668f65c9af87db774c42a2e52b55
Mark Windholtz <mark@agiledna.com>
2015-11-19 17:04:26 -0500

    better encapsulation of prez

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
+  protected
+    attr_reader   :prez
+    helper_method :prez  
+        
 end
