3c34f906b8bad6e4c2c05bac5dd9d887b0d364e6
Mark Windholtz <mark@agiledna.com>
2015-12-02 17:30:09 -0500

    added home_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'cargos#index'
+  resources :home, only: :index
+  root 'home#index'
   resources :cargos
   resources :locations
 
